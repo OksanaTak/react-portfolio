b0cf6df27e416dc40d6a88f9647e79392f3ecb73
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-10-21 09:54:46 -0700

    added social media icons

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,24 +1,28 @@
 import React from 'react'
+import './contact.css'
+
 let linkedin = '/assets/images/linkedin.jpeg';
-let github = '/assets/images/linkedin.jpeg';
-let email = '/assets/images/linkedin.jpeg';
+let github = '/assets/images/github.png';
+let email = '/assets/images/gmail.png';
 
 export default function Contact () {
   return (
-    <div>
+    <div className='text-center'>
       <h1>Contact Page</h1>
-
+<p>
+  Please, stay in touch! Contact me!
+</p>
     
         <a target='_blank' href='https://www.linkedin.com/in/oksana-tatsyak-048b0727a/'>
-        <img height="10px" width= "10px" src={process.env.PUBLIC_URL + linkedin} alt="linkedin" />
+        <img className='icons' src={process.env.PUBLIC_URL + linkedin} alt="linkedin" />
         </a>
     
     
         <a target='_blank' href='https://github.com/OksanaTak'>
-        <img height="10px" width= "10px" src={process.env.PUBLIC_URL + github} alt="github" />        </a>
+        <img className='icons' src={process.env.PUBLIC_URL + github} alt="github" />        </a>
      
         <a target='_blank' href='mailto:oksana.tatsyak@gmail.com'>
-        <img height="10px" width= "10px" src={process.env.PUBLIC_URL + email} alt="email" />        </a>
+        <img className='icons' src={process.env.PUBLIC_URL + email} alt="email" />        </a>
   
     </div>
   )
